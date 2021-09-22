8b58f5155f1d010bbc18621858d5fd58ef63cc77
Alan <alanjosedossantos011@gmail.com>
2021-09-22 16:24:29 -0300

    initial comit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json())
+app.use(express.json())
+
+
+app.listen(3000, () => {
+    console.log('Server started at http://localhost:3000');
+
+})
