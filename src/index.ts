1e743995416087eb469ddfa3155e39f956395af1
Alan <alanjosedossantos011@gmail.com>
2021-09-23 10:20:15 -0300

    configuracao typeorm, banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express from 'express'
+import 'reflect-metadata';
 import routes from './routes';
+import './database/connect'
+import express from 'express'
 
 const app = express();
 
 app.use(express.json())
-app.use(express.json())
+app.use(routes)
 
 
 app.listen(3000, () => {
